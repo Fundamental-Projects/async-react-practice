09e1ed6ef668f2003b10c757cf4b48e352bc3451
TolgaArslann <t0lg4.urun@gmail.com>
2026-08-11 16:30:39 +0300

    first commit-api project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+import { fetchApi, fetchSearchApi } from "./services/fetchApi";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ src/services/fetchApi.js
@@ -0,0 +1,33 @@
+const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+const BASE_URL = "https://api.giphy.com/v1/gifs/";
+
+async function requestGiphy(endpoint, params = {}) {
+  const url = new URL(endpoint, BASE_URL);
+
+  url.searchParams.set("api_key", API_KEY);
+  url.searchParams.set("limit", "10");
+
+  Object.entries(params).forEach(([key, value]) => url.searchParams.set(key, value));
+
+  const request = new Request(url, {
+    method: "GET",
+  });
+
+  const response = await fetch(request);
+
+  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+  return response.json();
+}
+
+function fetchTrendingApi() {
+  return requestGiphy("trending");
+}
+
+function fetchSearchApi(searchTerm) {
+  return requestGiphy("search", {
+    q: searchTerm,
+  });
+}
+
+export { fetchTrendingApi, fetchSearchApi };
